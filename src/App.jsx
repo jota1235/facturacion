c81f7e812e8abcfbf003a4085e026dc7b546a673
jota1235 <jafetchidi@gmail.com>
2025-08-22 13:15:57 -0600

    Descripción de los cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 import { ThemeProvider } from './theme/ThemeProvider'; // <<— del paso anterior
 import Navbar from './components/navbar';
